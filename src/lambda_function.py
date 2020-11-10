6e2285218cd81feb681e9c5665be3859ae5f319a
axelleysen <axel@leysenfamily.co.uk>
2020-11-10 20:36:54 +0000

    updating lambda for SAM

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify 
-app = Flask(__name__)
+import logging
+import json
 
-# Defining route and function for api
-@app.route('/total/<string:numbers_to_add>')
-def sum_list(numbers_to_add):
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
 	"""Return the sum of a list of numbers.
 
 	Argument -- [String] Comma separated integers
@@ -13,12 +15,14 @@ def sum_list(numbers_to_add):
 	}
 
 	"""
+	logging.info("Incoming event: %s" % event)
+	numbers_to_add = event['queryStringParameters']['list']
 
 	# Converting api inputs into list
 	list_to_add = [int(item) for item in numbers_to_add.split(",")]
-	# Return response as json object
-	return jsonify({"total": sum(list_to_add)})
+	total = sum(list_to_add)
 
-# Initialising flask application
-if __name__ == '__main__':
-	app.run(debug=True)
+	# Return response as json object
+	logging.info("Total output: %s" % total)
+	return {"total": total}
+
